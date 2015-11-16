f2360ccaa60475fb18a0d929d1a36b4c5f1fa593
webremote <webremote@users.noreply.github.com>
2015-11-17 00:59:32 +0100

    Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# php-remote-client
-WebRemote's PHP Client - Manage your webserver
+# PHP Remote: Client
+WebRemote's PHP Client - Manage your webserver, secure. Save usage of Remote PHP Execution is possible! With our Open Source remote PHP client, you can access, check and manage your webserver through PHP.
+
+Check out https://www.webremote.io for more information.
---
+++ webremote.php
@@ -0,0 +1,62 @@
+<?php
+/*
+This file is part of WebRemote.io PHP Remote Client.
+
+    WebRemote.io PHP Remote Client is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebRemote.io PHP Remote Client is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebRemote.io PHP Remote Client.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * webremote.php - PHP Remote Client
+ *
+ * Version: 1.0.1001
+ * 
+ * For details: https://webremote.io/php-remote-client
+ */
+
+// Define authentication key, automatically generated or replaced by you
+define('AUTHENTICATION', 'STRINGTHATISGOINGTOBEREPLACED');
+
+// Setup headers
+header('X-Powered-By: WebRemote/1.0');
+header('Content-type: application/json');
+
+// Check if the Authentication-header is correct
+if (empty($_SERVER['WEBREMOTE']) || $_SERVER['WEBREMOTE'] = AUTHENTICATION) {
+	exit('[]');
+}
+
+// Receive the task
+if (!empty($_POST['task'])) {
+
+	// Write to a temporary file
+	$tempFP = tmpfile();
+	fwrite($tempFP, $_POST['task']);
+
+	// Start capturing output
+	ob_start();
+	include($tempFP);
+	$response = file_get_contents();
+
+	// Remote the task
+	fclose($tempFP);
+
+	// Output the task result
+	if (is_array($response)) {
+		$response = json_encode($response);
+	}
+	header('Content-length: ' . strlen((string)$response));
+	exit($response);
+}
+header('Location: http://' . $_SERVER['REMOTE_HOST'] . '.weblets.webremote.io/');
+exit;
